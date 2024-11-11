65451a0d2e98396716f4b542ddab64e85167fec1
Mehul <mehulrastogi2002@gmail.com>
2024-11-11 17:26:26 -0500

    add data

--- processing/clean.py
+++ processing/clean.py
@@ -47,12 +47,32 @@ def create_data_model(relevant_courses):
         }
     return clean_data_model
 
+def special_topics_design(clean_data_model):
+    special_topics_classes = {"CX 4803", "CS 4803", "CS 8803", "CSE 4803", "CSE 8803", "ECE 8803", "ECE 4803"}
+    new_entries = {}
+    for key in clean_data_model.keys():
+        if key in special_topics_classes:
+            all_section_info = clean_data_model[key]["Section Information"]
+            for section in all_section_info.keys(): 
+                new_entries[key + "-" + section] = {
+                    "Name": "",
+                    "Description": "",
+                    "Section Information": {
+                        section: all_section_info[section]
+                    }
+                }
+    for sp_class in special_topics_classes:
+        if sp_class in clean_data_model.keys():
+            del clean_data_model[sp_class]
+    clean_data_model = clean_data_model | new_entries
+    return clean_data_model
 
 if __name__ == "__main__":
     filePath = "../source/data.json"
     data = read_data(filePath)
     relevant_courses = clean_data_relevant_courses(data)
     clean_data_model = create_data_model(relevant_courses)
+    clean_data_model = special_topics_design(clean_data_model)
 
     with open("data_clean.json", "w") as json_file:
         json.dump(clean_data_model, json_file, indent=4)
