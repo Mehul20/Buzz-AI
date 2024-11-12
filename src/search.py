6e04a654663ea430784b640b7553ae0f9ad76eb4
Mehul <mehulrastogi2002@gmail.com>
2024-11-11 19:11:14 -0500

    most of the logic done

--- src/search.py
+++ src/search.py
@@ -1,16 +1,28 @@
 import faiss
 from sentence_transformers import SentenceTransformer
+from utils import read_file
 import numpy as np
 
-def similarity_for_query(query_string):
+def similarity_for_query(user_query):
     model = SentenceTransformer('all-MiniLM-L6-v2')
     index = faiss.read_index("../Processed/faiss_index.index")
-    course_ids = np.load("./Processed/course_ids.npy", allow_pickle=True)
+    course_ids = np.load("../Processed/course_ids.npy", allow_pickle=True)
 
-    vectorized_query = model.encode(query_string).astype("float32").reshape(1, -1)
+    vectorized_query = model.encode(user_query).astype("float32").reshape(1, -1)
     _, indices = index.search(vectorized_query, 2)
-    topKIndices = indices[0].split(" ")
-    print(topKIndices)
-    print(course_ids[int(topKIndices[0])])
 
-similarity_for_query("computer archietecture")
+    topKIndices = list(indices[0])
+    top_results = []
+    for index in topKIndices:
+        top_results.append(course_ids[index])
+    return top_results
+
+def convert_top_results_into_data(top_results):
+    data = read_file("../Processing/data_clean.json")
+    for curr_result in top_results:
+        class_name = data[curr_result]["Name"]
+        print(curr_result, class_name)
+
+user_query = "computer archietecture"
+top_results = similarity_for_query(user_query)
+convert_top_results_into_data(top_results)
