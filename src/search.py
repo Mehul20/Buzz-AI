0a0b0c2d2bb56e8257569e019a9f50e25545d69f
Mehul <mehulrastogi2002@gmail.com>
2024-11-16 00:01:09 -0500

    implement subject filtering

--- src/search.py
+++ src/search.py
@@ -9,7 +9,7 @@ def similarity_for_query(user_query):
     course_ids = np.load("../Processed/course_ids.npy", allow_pickle=True)
 
     vectorized_query = model.encode(user_query).astype("float32").reshape(1, -1)
-    _, indices = index.search(vectorized_query, 10)
+    _, indices = index.search(vectorized_query, 15)
 
     topIndices = list(indices[0])
     top_results = []
@@ -17,12 +17,15 @@ def similarity_for_query(user_query):
         top_results.append(course_ids[index])
     return top_results
 
-def convert_top_results_into_data(top_results):
+def convert_top_results_into_data(top_results, subject):
     data = read_file("../Processing/data_clean.json")
     for curr_result in top_results:
         class_name = data[curr_result]["Name"]
-        print(curr_result, class_name)
+        curr_subject = curr_result.split(" ")[0]
+        if len(subject) == 0 or curr_subject in subject:
+            print(curr_result, class_name)
 
-user_query = "Crypto"
+user_query = "Cryptography"
+subject = ["CS"]
 top_results = similarity_for_query(user_query)
-convert_top_results_into_data(top_results)
+convert_top_results_into_data(top_results, subject)
