71e20e3958a145186d6146facd1ccbe374e007eb
Mehul <mehulrastogi2002@gmail.com>
2024-11-17 15:09:52 -0500

    training done

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,16 @@ def read_file(filePath):
         data = json.load(file)
     return data
 
+def check_relevant_special_topics(course):
+    init_split = course.split(" ")
+    subject = init_split[0]
+    split2 = init_split[1].split("-")
+    number = split2[0]
+    compatible_courses = {"CS", "CSE", "CM", "CX"}
+    if subject in compatible_courses and (number == "4803" or number == "8803"):
+        return True, split2[1]
+    return False, None
+
 # Unused - Might need them in the future
 def save_pickle_dictionary(dictionary, filePath):
     with open(filePath, "wb") as file:
